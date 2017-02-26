c0bee0b6993719a146e543853cf20de32f97a4fc
yagnimurali <yagni99@gmail.com>
2017-02-26 21:18:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 alert("Welcome! Thank you for visiting my website...!");
 var counter=0;
 var button = document.getElementById('like');
-button.onClick= function() {
+button.onClick = function() {
     counter=counter+1;
     var span = document.getElementById('value');
     span.innerHTML=counter.toString();
