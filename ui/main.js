0bb44a34f274e541c72ab045e423a7c07bac98d7
yagnimurali <yagni99@gmail.com>
2017-02-26 20:42:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,9 @@
 console.log('Loaded!');
 alert("Welcome! Thank you for visiting my website...!");
+var likes=0;
+var like = grtelementbyId("like");
+like.onClick= function() {
+    likes=likes+1;
+    var span = getelementbyId("value");
+    span.innerHTML=likes;
+    };
