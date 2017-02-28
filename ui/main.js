21da8c37b1a605a9daf23dda9f4d077fc1623285
yagnimurali <yagni99@gmail.com>
2017-02-28 09:02:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-//alert("Welcome! Thank you for visiting my website...!");
+alert("Welcome! Thank you for visiting my website...!");
 
 var button = document.getElementById('counter');
 var counter=0;
