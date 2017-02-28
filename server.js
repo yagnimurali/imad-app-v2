ffa99e8023b530cb39392fa3ee1f582be465d030
yagnimurali <yagni99@gmail.com>
2017-02-28 10:29:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get('/article-three', function (req,res){
     res.sendFile(path.join(__dirname, 'Article-three.html'));
 });
 
+app.get('/cresh', function (req,res){
+    res.sendFile(path.join(__dirname, 'cresh.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
