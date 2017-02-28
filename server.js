db0bea81d02a5b82ce4fdfb2c59ae929acf92b6f
yagnimurali <yagni99@gmail.com>
2017-02-28 09:25:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req,res){
-    res.send("this is the article one");
+ res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
 app.get('/article-two', function (req,res){
