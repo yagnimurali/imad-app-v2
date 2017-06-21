08cdac6020586e6b652dc3e5d6b26f7bb2b39bb8
yagnimurali <yagni99@gmail.com>
2017-06-20 18:32:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 //alert("Welcome! Thank you for visiting my website..
 var element = document.getElementById("sodi");
 element.innerHTML = "just a check";
+var img = document.getElementById('leftbar');
+img.onClick= function (){
+    img.style.marginleft = "100px";
+};
