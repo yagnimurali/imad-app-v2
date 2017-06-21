b2c8bfad40b809444fa4fea7377b589fa5650924
yagnimurali <yagni99@gmail.com>
2017-06-20 19:14:33 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,10 @@ console.log('Loaded!');
 //alert("Welcome! Thank you for visiting my website..
 var element = document.getElementById("sodi");
 element.innerHTML = "just a check";
-var img = document.getElementById('leftbar');
-img.onClick= function (){
-    img.style.marginLeft='100px';
+var button = document.getElementById('count');
+var counter =0;
+button.onClick =function(){
+    counter=counter+1;
+    var value=document.getElementById('counterv');
+    value.innerHTML='counter.toString()';
 };
