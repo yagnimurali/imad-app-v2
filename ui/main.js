b4badba4297f104bdcc741248cfa61fbf834d353
yagnimurali <yagni99@gmail.com>
2017-06-20 19:16:31 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ var counter =0;
 button.onClick =function(){
     counter=counter+1;
     var value=document.getElementById('counterv');
-    value.innerHTML="counter.toString()";
+    value.innerHTML=counter.toString();
 };
