7801ef32190297edbb1d8369faa009f95416b36e
yagnimurali <yagni99@gmail.com>
2017-06-20 20:56:32 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,22 @@ console.log('Loaded!');
 //alert("Welcome! Thank you for visiting my website..
 var element = document.getElementById("sodi");
 element.innerHTML = "just a check";
+
+
 var button = document.getElementById('coun');
 var counter =0;
-button.onclick = function (){
-    counter=counter+1;
-    var value=document.getElementById('counterv');
-    value.innerHTML=counter.toString();
-    
+
+var request = new XMLHttpRequest();
+request.onreadystatechange=function(){
+    if(reauest.readystate === XMLHttpRequest.DONE)
+    {
+        if(reauest.status === 200)
+        {
+             var counter=request.responseText;
+             var value=document.getElementById('counterv');
+             value.innerHTML=counter.toString();
+        }
+    }
 };
 
+request.open('GET','http://yagnimurali.imad.hasura-app.io/counter',true);
