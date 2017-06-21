2fe15d2b1cceb0bc545634f37a5c2d3a6cd8f0ce
yagnimurali <yagni99@gmail.com>
2017-06-20 21:05:50 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,20 +5,21 @@ element.innerHTML = "just a check";
 
 
 var button = document.getElementById('coun');
-var counter =0;
-
-var request = new XMLHttpRequest();
-request.onreadystatechange=function(){
-    if(reauest.readyState === XMLHttpRequest.DONE)
-    {
-        if(reauest.status === 200)
+ button.onclick= function()
+     {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange=function()
         {
-             var counter=request.responseText;
-             var value=document.getElementById('counterv');
-             value.innerHTML=counter.toString();
-        }
-    }
-};
-
+         if(reauest.readyState === XMLHttpRequest.DONE)
+         {
+             if(reauest.status === 200)
+                {
+                    var counter=request.responseText;
+                     var value=document.getElementById('counterv');
+                     value.innerHTML=counter.toString();
+                }
+         }
+        };
+    };
 request.open('GET','http://yagnimurali.imad.hasura-app.io/counter',true);
 request.send(null);
