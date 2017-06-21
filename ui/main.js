233f09f39a553cb52d71bd2ff11590bb6ff16e3d
yagnimurali <yagni99@gmail.com>
2017-06-21 10:23:10 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ request.send(null);
 
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('sub');
 
 submit.onclick=function(){
     /*var names=['name1', 'name2', 'name3', 'name4'];
