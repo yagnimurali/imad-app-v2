800041ce18747f0f42b473ddbcd94f74fa4ff651
yagnimurali <yagni99@gmail.com>
2017-06-22 09:04:45 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,18 +25,17 @@ request.open('GET','http://yagnimurali.imad.hasura-app.io/counter',true);
 request.send(null);
 };
 
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
-var submit = document.getElementById('submit_btn');
+//submit
 
-submit.onclick = function()
-{
-    var names=['name1', 'name2', 'name3', 'name4'];
-    var list='';
-    for (var i=0;i < names.length; i++)
-    {
-        list += '<li>'+ names[i] +'</li>';
-    }
- var ul=document.getElementById('namelist');
- ul.innerHTML=list;
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick=function(){
+  names = ['name1','name2','name3','name4'];
+  list = '';
+  for (var i =0; i<names.length; i++){
+      list += '<li>' + names[i] + '</li>';
+      }
+var ul=document.getElementById('namelist');
+ul.innerHTML = list;
 };
