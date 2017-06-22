9c8c1996833f8bd323eb0f983c07fc52ed48dc27
yagnimurali <yagni99@gmail.com>
2017-06-22 08:05:34 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,16 +27,16 @@ request.send(null);
 
 var nameInput=document.getElementById("name");
 var name=nameInput.value;
-var button = document.getElementById('sub');
+var submit = document.getElementById('submit_btn');
 
 button.onclick = function()
 {
-    /*var names=['name1', 'name2', 'name3', 'name4'];
+    var names=['name1', 'name2', 'name3', 'name4'];
     var list='';
     for (var i=0;i < names.length; i++)
     {
         list += '<li>'+ names[i] +'</li>';
-    }*/
- var span=document.getElementById("spa");
- span.innerHTML="list";
+    }
+ var ul=document.getElementById('namelist');
+ ul.innerHTML=list;
 };
